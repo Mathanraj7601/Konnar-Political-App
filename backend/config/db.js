70b2c69283db6dbbdf152df2bf41785890adf39c
MATHAN <mathanmathan8870@gmail.com>
2026-04-10 16:40:28 +0530

    remove env

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,7 +2,7 @@ const mysql = require('mysql2/promise');
 require('dotenv').config();
 
 const pool = mysql.createPool({
-  uri: process.env.DATABASE_URL || 'mysql://root:wdohFTvGcnBjqCAOtQZDeUaBrqNXQvBj@mysql-c3-d.railway.internal:3306/railway',
+  uri: process.env.DATABASE_URL || 'mysql:/:3306/railway',
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0
